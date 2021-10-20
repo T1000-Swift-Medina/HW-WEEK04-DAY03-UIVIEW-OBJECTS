f99e50220e663f5ec94045c896b46e148549de86
AfafSaleh <Afaf.Saleh.med.s@tuwaiq.edu.sa>
2021-10-20 23:53:14 +0300

    HW-GOO

---
+++ GoodMorning/GoodMorning/ViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  GoodMorning
+//
+//  Created by العــفاف . on 13/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    @IBOutlet weak var Unlock: UILabel!
+    
+    @IBOutlet weak var EnterName: UITextField!
+    
+    @IBAction func PhoneUnlock(_ sender: Any) {
+        if (EnterName.text == "Afaf"){
+            Unlock.text = "Unlocked ✅"
+        } else {
+            Unlock.text = "Not Unlocked ❌"
+        }
+    }
+    @IBOutlet weak var AccessD: UILabel!
+    
+        @IBAction func OpenYouTube(_ sender: Any) {
+            if (EnterName.text == "Afaf"){
+                AccessD.text = "Allowed ✅"
+            } else{
+                AccessD.text = "Access Denied ❌ "
+            }
+        }
+    
+    @IBOutlet weak var Allowed: UILabel!
+    @IBAction func OpenMadrasti(_ sender: Any) {
+    if (EnterName.text == "Afaf"){
+        Allowed.text = "Allowed ✅"
+    } else {
+        Allowed.text = "Not Allowed ❌"
+    }
+    
+    }
+    
+   
+    @IBOutlet weak var AllowMad: UILabel!
+    
+    @IBOutlet weak var KidName: UITextField!
+   
+    
+    
+    
+    @IBAction func AccMAD(_ sender: Any) {
+        if(KidName.text == "Jawana"){
+            Allowed.text = "Allowed ✅"
+        } else {
+            Allowed.text = "Not Allowed ❌"
+        }
+        
+        }
+    
+    @IBAction func AccYT(_ sender: Any) {
+        if (KidName.text == "Jawana"){
+            AccessD.text = "Allowed ✅"
+        } else{
+            AccessD.text = "Access Denied ❌ "
+        }
+        }
+    }
