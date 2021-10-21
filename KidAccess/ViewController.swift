16569027f0fd43e4821b649faa23ebe68ddf47b2
RahafAbdulaal <Rahaf.Abdulaal.med.s@tuwaiq.edu.sa>
2021-10-21 12:28:41 +0300

    Initial Commit

---
+++ KidAccess/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KidAccess
+//
+//  Created by Rahaf Abdulaal on 14/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
