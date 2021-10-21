cfdc36baecbb9532316add850be3680f6730eb94
nada_alansari <nada0an90@gmail.com>
2021-10-21 12:13:26 +0300

    fix empty error

--- HW-WEEK04-DAY-3/ViewController.swift
+++ HW-WEEK04-DAY-3/ViewController.swift
@@ -3,54 +3,70 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var labl1: UILabel!
     @IBOutlet weak var labl2: UILabel!
     @IBOutlet weak var lbl3: UILabel!
     @IBOutlet weak var nameText: UITextField!
     @IBOutlet weak var kidName: UITextField!
-
+    
     @IBOutlet weak var accYoutube: UISwitch!
-        @IBOutlet weak var accMadrasati: UISwitch!
+    @IBOutlet weak var accMadrasati: UISwitch!
     
-    
-    @IBAction func unlock(_ sender: Any) {
-        
-        
-        if (nameText.text == kidName.text) {
-                labl1.text = "Unlocked ✅"
-                  }
-            else {
-                labl1.text = "locked  ❌"
-                  }
-    }
-
-
-    @IBAction func openYoutupe(_ sender: Any) {
-        if (accYoutube.isOn == true && nameText.text == kidName.text) {
-                  labl2.text = "Allowed ✅"
-              }
-        else {
-                  labl2.text =  "Accses Dined ❌"
-              }
-    }
-    
-    @IBAction func openMadrasati(_ sender: Any) {
-        
-        if (accMadrasati.isOn == true && nameText.text == kidName.text) {
-                  lbl3.text = "Allowed ✅"
-              }
-        else {
-                  lbl3.text =  "Accses Dined ❌"
-              }
-
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func unlock(_ sender: Any) {
+        
+        if(nameText.text == ""){
+            labl1.text = "Please enter your name"
+            
+        }
+        else if (nameText.text == kidName.text) {
+            labl1.text = "Unlocked ✅"
+        }
+        
+        else {
+            labl1.text = "locked  ❌"
+        }
+    }
+    
+    
+    @IBAction func openYoutupe(_ sender: Any) {
+        if(nameText.text == ""){
+            labl2.text = "Please enter your name"
+            
+        }
+        else if (accYoutube.isOn == true && nameText.text == kidName.text) {
+            labl2.text = "Allowed ✅"
+        }
+        else {
+            labl2.text =  "Accses Dined ❌"
+        }
+    }
+    
+    @IBAction func openMadrasati(_ sender: Any) {
+         if(nameText.text == ""){
+            lbl3.text = "Please enter your name"
+            
+        }
+        
+        else if (accMadrasati.isOn == true && nameText.text == kidName.text) {
+            lbl3.text = "Allowed ✅"
+        }
+   
+        else {
+            lbl3.text =  "Accses Dined ❌"
+        }
+        
+    }
+    
+    
+    
+    
 }
 
