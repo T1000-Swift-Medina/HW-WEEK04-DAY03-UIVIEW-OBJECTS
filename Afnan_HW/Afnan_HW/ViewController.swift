0012382bf2c6db742adc521e94dbfd0d29236c08
AfnanTheb <Afnan.Alotaibi.med.s@tuwaiq.edu.sa>
2021-10-21 13:21:13 +0300

    done write ViewController

--- Afnan_HW/Afnan_HW/ViewController.swift
+++ Afnan_HW/Afnan_HW/ViewController.swift
@@ -9,6 +9,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameUser: UITextField!
+    @IBOutlet weak var labelOne: UILabel!
+    @IBOutlet weak var labelTow: UILabel!
+    @IBOutlet weak var labelThree: UILabel!
+    
+    @IBOutlet weak var switchYoutube: UISwitch!
+    @IBOutlet weak var switchMadrasati: UISwitch!
+    
+    @IBOutlet weak var nameKid: UITextField!
+    
+    @IBAction func unlockPhone(_ sender: Any) {
+        if (nameKid.text == "" || nameUser.text == "")
+        { labelOne.text = "Access Denied"
+         
+            
+        }else {
+        if (nameKid.text == nameUser.text)
+        {
+            labelOne.text = "Allowed"
+        }else {
+            labelOne.text = "Access Denied"
+        }
+    }
+    }
+    
+    @IBAction func openYoutube(_ sender: Any) {
+        if (switchYoutube.isOn)
+        {
+            labelTow.text = "Allowed"
+        }else {
+            labelTow.text = "Access Denied"
+        }
+    }
+    
+    @IBAction func openMadrasati(_ sender: Any) {
+        if (switchMadrasati.isOn)
+        {
+            labelThree.text = "Allowed"
+        }else {
+            labelThree.text = "Access Denied"
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
