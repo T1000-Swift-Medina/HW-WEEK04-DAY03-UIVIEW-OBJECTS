06bb78e73024f658e9d062b5f290fa4a11d8e019
shroog <shroogalhwity@gmail.com>
2021-10-28 11:46:35 +0300

    add change

--- C1/C1/ViewController.swift
+++ C1/C1/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var yourname: UITextField!
     
@@ -16,6 +16,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var statusYoutube: UILabel!
     
     @IBOutlet weak var statusMadrasti: UILabel!
+    
+    @IBOutlet weak var madrastiSwich: UISwitch!
+    
+    @IBOutlet weak var youtubeSwich: UISwitch!
+    
+    
+    @IBOutlet weak var kidName: UITextField!
+    
     
     @IBAction func unlockPhone(_ sender: Any) {
         if (yourname.text == "" || kidName.text == "") {statusUnlockPhone.text = "locked❌"
@@ -55,18 +63,52 @@ class ViewController: UIViewController {
         
         
     }
-    @IBOutlet weak var madrastiSwich: UISwitch!
     
-    @IBOutlet weak var youtubeSwich: UISwitch!
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        UserDefaults.standard.set(kidName.text, forKey: "TextName")
+        
+    }
+    
+   // @IBOutlet weak var madrastiSwich: UISwitch!
+    
+   // @IBOutlet weak var youtubeSwich: UISwitch!
     
     
-    @IBOutlet weak var kidName: UITextField!
+   // @IBOutlet weak var kidName: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        kidName.delegate = self
         // Do any additional setup after loading the view.
+       // UserDefaults.standard.set(youtubeSwich.isOn, forKey: "Youtube")
+       // UserDefaults.standard.set(madrastiSwich.isOn,forKey: "Madrasti")
+        //UserDefaults.standard.set(kidName.text, forKey: "kidName")
+        
+        let isYoutubeAllowed = UserDefaults.standard.bool(forKey: "Youtube")
+    youtubeSwich.isOn = isYoutubeAllowed
+        let isMadrastiAllowed = UserDefaults.standard.bool(forKey: "Madrasti")
+        madrastiSwich.isOn = isMadrastiAllowed
+        
+        let iskidName = UserDefaults.standard.string(forKey: "TextName")
+        kidName.text = iskidName
+        
     }
 
-
+    @IBAction func onMadrasati(_ sender: Any) {
+        UserDefaults.standard.set(madrastiSwich.isOn,forKey: "Madrasti")
+    }
+  
+    
+    @IBAction func onYutube(_ sender: Any) {
+        UserDefaults.standard.set(youtubeSwich.isOn, forKey: "Youtube")
+        
+    }
+    
+//    @IBAction func textkid(_ sender: Any) {
+//        UserDefaults.standard.set(kidName.text, forKey: "TextName")
+//
+//    }
+    
 }
 
