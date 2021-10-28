5b162e01a7422d6716c1697db49218b2c2572355
AfafSaleh <Afaf.Saleh.med.s@tuwaiq.edu.sa>
2021-10-28 10:25:22 +0300

    HW: 28 Oct

--- GoodMorning/ViewController.swift
+++ GoodMorning/ViewController.swift
@@ -11,7 +11,6 @@ class ViewController: UIViewController {
 
     
     @IBOutlet weak var switchMad: UISwitch!
-    
     @IBOutlet weak var SwitchYOUT: UISwitch!
     @IBOutlet weak var Unlock: UILabel!
     @IBOutlet weak var EnterName: UITextField!
@@ -20,6 +19,9 @@ class ViewController: UIViewController {
    
     
     @IBAction func PhoneUnlock(_ sender: Any) {
+        
+        UserDefaults.standard.set(KidName.text, forKey: "Name")
+        
         if (EnterName.text == "Afaf"){
             Unlock.text = "Unlocked ✅"
         } else {
@@ -29,6 +31,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var AccessD: UILabel!
     
         @IBAction func OpenYouTube(_ sender: Any) {
+            
+            UserDefaults.standard.set(SwitchYOUT.isOn, forKey: "YouTupe")
+            
             if (SwitchYOUT.isOn){
                 AccessD.text = "Allowed ✅"
             } else{
@@ -40,6 +45,9 @@ class ViewController: UIViewController {
     
     
     @IBAction func OpenMadrasti(_ sender: Any) {
+        
+        UserDefaults.standard.set(switchMad.isOn, forKey: "Madrasa")
+        
         if (switchMad.isOn){
         Allowed.text = "Allowed ✅"
     } else {
@@ -47,7 +55,23 @@ class ViewController: UIViewController {
     }
     
     }
-    
-   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //
+        //UserDefaults.standard.set(SwitchYOUT.isOn, forKey: "YouTupe")
+      //  UserDefaults.standard.set(switchMad.isOn, forKey: "Madrasa")
+    //    UserDefaults.standard.set(KidName.text, forKey: "Jwana")
+       
+        let isYouTupAllowed = UserDefaults.standard.bool(forKey: "YouTupe")
+        SwitchYOUT.isOn = isYouTupAllowed
+        
+        let  isMadrasaAllowed = UserDefaults.standard.bool(forKey: "Madrasa")
+        SwitchYOUT.isOn = isMadrasaAllowed
+        
+        let isName = UserDefaults.standard.object(forKey: "Name")
+        KidName.text = isName as? String
+        //isname.text = as? String
+        
+    }
 
     }
