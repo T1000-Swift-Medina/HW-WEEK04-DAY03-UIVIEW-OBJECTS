fde501524e7fbca0b8d35bf514a511745279eb0d
reem alsubhi <Reem.Alsubi.med.s@tuwaiq.edu.sa>
2021-10-28 14:28:59 +0300

    HomeWork

--- home workk/home workk/ViewController.swift
+++ home workk/home workk/ViewController.swift
@@ -7,20 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
     @IBOutlet weak var FullName: UITextField!
     
     @IBOutlet weak var Unlock: UILabel!
+    
+    @IBOutlet weak var Access: UILabel!
+    
+    @IBOutlet weak var Allowed: UILabel!
+    
+    @IBOutlet weak var KidName: UITextField!
+    
+    @IBOutlet weak var swichmad: UISwitch!
+    
+    @IBOutlet weak var swichyou: UISwitch!
+    
     @IBAction func pHONE(_ sender: Any) {
         if (FullName.text == "Reem"){
             Unlock.text = "Unlock ✅"
         } else {
             Unlock.text = "Not Unlock ❌"
         }
+        
     }
     
-    @IBOutlet weak var Access: UILabel!
     
     @IBAction func Open(_ sender: Any) {
         if (FullName.text == "Reem"){
@@ -29,7 +40,7 @@ class ViewController: UIViewController {
             Access.text = "Not Access Denied ❌"
         }
     }
-    @IBOutlet weak var Allowed: UILabel!
+    
     
     
     @IBAction func Madrasati(_ sender: Any) {
@@ -38,11 +49,20 @@ class ViewController: UIViewController {
         } else {
             Allowed.text = "Not Allowed ❌"
         }
+    }
     
-   
-}
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        UserDefaults.standard.set(KidName.text, forKey: "TextName")
+    }
     
-    @IBOutlet weak var KidName: UITextField!
+    @IBAction func onmadrasti(_ sender: Any) {
+        UserDefaults.standard.set(swichmad.isOn,forKey: "Madrasti")
+        
+    }
+    
+    @IBAction func swichyou(_ sender: Any) {
+        UserDefaults.standard.set(swichyou.isOn, forKey: "Youtube")
+    }
     
     @IBAction func SwichMD(_ sender: UISwitch) {
         if (KidName.text == "Fars"){
@@ -50,8 +70,8 @@ class ViewController: UIViewController {
         } else {
             Allowed.text = "Not Allowed ❌"
         }
-    
-        }
+        
+    }
     
     @IBAction func SwichYU(_ sender: Any) {
         if (KidName.text == "Reem"){
@@ -59,9 +79,20 @@ class ViewController: UIViewController {
         } else {
             Access.text = "Not Access Denied ❌"
         }
-        }
     }
     
     
-    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        KidName.delegate = self
+        
+        let isYoutubeAllowed = UserDefaults.standard.bool(forKey: "Youtube")
+        swichyou.isOn = isYoutubeAllowed
+        let isMadrastiAllowed = UserDefaults.standard.bool(forKey: "Madrasti")
+        swichmad.isOn = isMadrastiAllowed
+        let iskidName = UserDefaults.standard.string(forKey: "TextName")
+        KidName.text = iskidName
+        
+    }
+}
 
