26aaccb9455d57baedf9f66b10b9b86671018655
mohammad-alsharif <mohammad.alsharif.med.s@tuwaiq.edu.sa>
2021-10-28 14:59:19 +0300

    Homework

--- HomeWork/ViewController.swift
+++ HomeWork/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let userDefaults = UserDefaults()
+    
     @IBOutlet weak var yourName: UITextField!
     @IBOutlet weak var kidName: UITextField!
     @IBOutlet weak var madrasati: UISwitch!
@@ -16,13 +18,20 @@ class ViewController: UIViewController {
     @IBOutlet weak var lbl: UILabel!
     @IBOutlet weak var lbl2: UILabel!
     @IBOutlet weak var lbl3: UILabel!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        yourName.text = userDefaults.string(forKey: "Mohammed")
+        youtube.isOn = userDefaults.bool(forKey:"Youtube")
+        madrasati.isOn = userDefaults.bool(forKey:"Madrasa")
+        kidName.text = userDefaults.string(forKey: "Mohammed")
+
     }
     
     @IBAction func unlockPhone(_ sender: UIButton) {
+        
+        userDefaults.set(yourName.text, forKey: "Mohammed")
         
         if yourName.text == kidName.text {
             lbl.text = "Unlocked ✅"
@@ -32,17 +41,26 @@ class ViewController: UIViewController {
     }
     
     @IBAction func openYoutube(_ sender: UIButton) {
-        if youtube.isOn {
+        
+        userDefaults.set(youtube.isOn, forKey: "Youtube")
+        
+        if youtube.isOn == true {
             lbl2.text = "Allowed ✅"
         } else {
         lbl2.text = "Not Allowed ❌"
         }
     }
+    
     @IBAction func openMadrasati(_ sender: UIButton) {
-        if madrasati.isOn {
+        
+        userDefaults.set(madrasati.isOn, forKey: "Madrasa")
+        
+        if madrasati.isOn == true {
             lbl3.text = "Allowed ✅"
+            
         } else {
             lbl3.text = "Not Allowed ❌"
         }
     }
+
 }
