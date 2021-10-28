707b6193c194a82be07958684fe1bb98f770cd90
Noura.alahmadi.med.s@tuwaiq.edu.sa <Teeko1122>
2021-10-28 13:50:47 +0300

    - add new Button to save the changes that made by user

--- example-layout02/example-layout02/ViewController.swift
+++ example-layout02/example-layout02/ViewController.swift
@@ -19,11 +19,12 @@ class ViewController: UIViewController , UITextViewDelegate {
     
     /*button*/
     @IBAction func unlockPhone(_ sender: Any) {
+        
         if(kidName.text == "" || enterKidName.text == ""){
             statusUnlockPhone.text = "Access Denied ❌"
             statusUnlockPhone.textColor = .red
         }
-       else if (kidName.text == enterKidName.text){
+        else if (kidName.text == enterKidName.text){
             statusUnlockPhone.text = "unlocked ✅"
             statusUnlockPhone.textColor = .green
         }else{
@@ -33,24 +34,61 @@ class ViewController: UIViewController , UITextViewDelegate {
     }
     
     @IBAction func youtubeButton(_ sender: Any) {
-        if(switchForYoutube.isOn == true){statusYoutube.text = "Time for some video..🎭"}
-        else{statusYoutube.text = "No Access for now ☹️"}
+        
+        if(switchForYoutube.isOn == true){statusYoutube.text = "Time for some video..🎭"
+            statusYoutube.textColor = .green
+        }
+        else{statusYoutube.text = "No Access for now ☹️"
+            statusYoutube.textColor = .red
+        }
+        
     }
     
     
     @IBAction func XcodeButton(_ sender: Any) {
-        if (switchForXcode.isOn == true){statusOpenXcode.text = "Time For Codeing..!!👨🏽‍💻"}
-        else{statusOpenXcode.text = "Take break ... 🧘🏽"}
+        
+        if (switchForXcode.isOn == true){statusOpenXcode.text = "Time For Codeing..!!👨🏽‍💻"
+            statusOpenXcode.textColor = .green
+        }
+        else{statusOpenXcode.text = "Take break ... 🧘🏽"
+            statusOpenXcode.textColor = .red
+        }
     }
     @IBAction func netflixButton(_ sender: Any) {
-        if(switchForNetflix.isOn){statusNetflix.text = "Movie Time 🍿"}
-        else{statusNetflix.text = "No Access for now ☹️"}
+        
+        if(switchForNetflix.isOn){statusNetflix.text = "Movie Time 🍿"
+            statusNetflix.textColor = .green
+        }
+        else{statusNetflix.text = "No Access for now ☹️"
+            statusNetflix.textColor = .red
+        }
     }
     
+    @IBAction func save(_ sender: Any) {
+        UserDefaults.standard.set(switchForXcode.isOn, forKey: "Xcode")
+        UserDefaults.standard.set(switchForYoutube.isOn, forKey: "youtube")
+        UserDefaults.standard.set(switchForNetflix.isOn, forKey: "netflix")
+        UserDefaults.standard.set(kidName.text, forKey: "kidName")
+    }
     
     override func viewDidLoad() {
-        
         super.viewDidLoad()
+        
+        let isXcodeAllowed = UserDefaults.standard.bool(forKey: "Xcode")
+        switchForXcode.isOn = isXcodeAllowed
+        
+        let isYoutubeAllowed = UserDefaults.standard.bool(forKey: "youtube")
+        switchForYoutube.isOn = isYoutubeAllowed
+        
+        let isNetflixAllowed = UserDefaults.standard.bool(forKey: "netflix")
+        switchForNetflix.isOn = isNetflixAllowed
+        
+        let isKidNameAllowed = UserDefaults.standard.object(forKey: "kidName")
+        kidName.text = isKidNameAllowed as? String
+        
+        
+        
+        
     }
 }
 
